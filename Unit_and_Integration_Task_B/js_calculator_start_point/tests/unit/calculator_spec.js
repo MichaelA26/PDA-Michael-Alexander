520bc0b7b7b2a10188e03e9314a20130330a6208
Michael Alexander <michael_alexander_20@outlook.com>
2019-11-14 15:00:00 +0000

    competed calc test

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -9,6 +9,62 @@ describe('calculator', function () {
   // write unit tests here in the form of "it should do something..."
   it('it has a sample test', function(){
     assert.equal(true, true)
-  })
+  });
+
+  it('it has to add', function(){
+    calculator.previousTotal = 4
+    calculator.add(6)
+    assert.strictEqual(calculator.runningTotal, 10)
+  });
+
+  it('it has to subtract', function(){
+    calculator.previousTotal = 10
+    calculator.subtract(4)
+    assert.strictEqual(calculator.runningTotal, 6)
+  });
+
+  it('it has to multiply', function(){
+    calculator.previousTotal = 6
+    calculator.multiply(2)
+    assert.strictEqual(calculator.runningTotal, 12)
+  });
+
+  it('it has to divide', function(){
+    calculator.previousTotal = 12
+    calculator.divide(3)
+    assert.strictEqual(calculator.runningTotal, 4)
+  });
+
+  it('it has to concatenate multiple number button clicks', function(){
+    calculator.runningTotal = 0
+    calculator.numberClick(1)
+    calculator.numberClick(0)
+    calculator.numberClick(1)
+    assert.strictEqual(calculator.runningTotal, 101)
+  });
+
+  it('it has to chain multiple operations together', function(){
+    calculator.numberClick(2)
+    calculator.operatorClick('+')
+    calculator.numberClick(2)
+    calculator.operatorClick('-')
+    calculator.numberClick(1)
+    calculator.operatorClick('*')
+    calculator.numberClick(5)
+    calculator.operatorClick('/')
+    calculator.numberClick(3)
+    calculator.operatorClick('=')
+    assert.strictEqual(calculator.runningTotal, 5)
+  });
+
+  it('it has to clear the running total without affecting calculation', function(){
+    calculator.numberClick(4)
+    calculator.operatorClick('+')
+    calculator.numberClick(7)
+    calculator.clearClick()
+    calculator.numberClick(7)
+    calculator.operatorClick('=')
+    assert.equal(calculator.runningTotal, '11')
+  });
 
 });
